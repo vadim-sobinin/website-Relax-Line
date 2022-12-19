4b3cabd4a5350ab7fd8e954c61128ae4b621aec0
vadim-sobinin <vadimsobinin@yandex.ru>
2022-12-19 23:46:25 +0300

    complete repairs type slider

--- src/modules/sliders.js
+++ src/modules/sliders.js
@@ -17,45 +17,59 @@ export const sliders = () => {
       prevEl: "#reviews-arrow_left",
     },
   });
-
+  const repairSliders = document.querySelector(".repair-types-slider").children;
   const repairNavList = document.querySelector(".nav-list-repair");
   const navButtons = repairNavList.querySelectorAll(".repair-types-nav__item");
 
-  // repairNavList.addEventListener("click", (e) => {
-  //   if (e.target.closest(".repair-types-nav__item")) {
-  //     navButtons.forEach((btn) => {
-  //       btn.classList.contains("active")
-  //         ? btn.classList.remove("active")
-  //         : null;
-  //     });
-
-  //     const selectedItemNumber = e.target
-  //       .closest(".repair-types-nav__item")
-  //       .classList[2].split("-")[3];
-
-  //     e.target.closest(".repair-types-nav__item").classList.add("active");
-
-  //     repairsSlider.$el = document.querySelector(
-  //       `.types-repair${selectedItemNumber}`
-  //     );
-  //   }
-  // });
-
   document.getElementById("repair-counter").style.zIndex = "10";
-  const repairsSlider = new Swiper(".types-repair1", {
-    navigation: {
-      nextEl: "#repair-types-arrow_right",
-      prevEl: "#repair-types-arrow_left",
-    },
-    pagination: {
-      el: "#repair-counter",
-      type: "fraction",
-      renderFraction: function (currentClass, totalClass) {
-        return `<div class="slider-counter-content">
-        <div class="slider-counter-content__current ${currentClass}"></div>
-        <div class="slider-counter-content__total ${totalClass}"></div>
-      </div>`;
+  const createSlider = (selector) => {
+    return new Swiper(selector, {
+      navigation: {
+        nextEl: "#repair-types-arrow_right",
+        prevEl: "#repair-types-arrow_left",
       },
-    },
+      pagination: {
+        el: "#repair-counter",
+        type: "fraction",
+        renderFraction: function (currentClass, totalClass) {
+          return `<div class="slider-counter-content">
+          <div class="slider-counter-content__current ${currentClass}"></div>
+          <div class="slider-counter-content__total ${totalClass}"></div>
+        </div>`;
+        },
+      },
+    });
+  };
+
+  const sliderArray = [
+    createSlider(".types-repair1"),
+    createSlider(".types-repair2"),
+    createSlider(".types-repair3"),
+    createSlider(".types-repair4"),
+    createSlider(".types-repair5"),
+  ];
+
+  repairNavList.addEventListener("click", (e) => {
+    if (e.target.closest(".repair-types-nav__item")) {
+      navButtons.forEach((btn) => {
+        btn.classList.contains("active")
+          ? btn.classList.remove("active")
+          : null;
+      });
+
+      const selectedItemNumber = e.target
+        .closest(".repair-types-nav__item")
+        .classList[2].split("-")[3];
+
+      e.target.closest(".repair-types-nav__item").classList.add("active");
+
+      sliderArray.forEach((slider, index) => {
+        slider.disable();
+        repairSliders[index].style.display = "none";
+      });
+      sliderArray[selectedItemNumber - 1].enable();
+      sliderArray[selectedItemNumber - 1].pagination.update();
+      repairSliders[selectedItemNumber - 1].style.display = "block";
+    }
   });
 };
